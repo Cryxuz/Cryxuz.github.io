a6503308f3b51f075e4d709814266b7336eb69c4
Paul David Tan <cryxuz@gmail.com>
2023-10-08 18:34:47 +1300

    home component done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,8 +4,9 @@ const Home = () => {
   return (
     <div>
       <div className="bg-fixed bg-center bg-cover text-center pt-[120px] md:p-[15%] xl:p-[10%]" style={{backgroundImage: `url(${home})`}}>
-        <h1 className='text-4xl font-extrabold'>Kia ora, I'm Paul.</h1>
-        <p>My goal is to provide users with memorable and efficient interactions."</p>
+        <h1 className='text-4xl md:text-5xl lg:text-6xl font-extrabold'>Kia ora, I'm Paul.</h1>
+        
+        <p className='text-md md:text-2xl lg:text-3xl'>My goal is to provide users with memorable and efficient interactions."</p>
       <div className="h-screen bg-opcity-75 flex justify-center"></div>
          
       </div>
