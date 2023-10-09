1236328de829bd951c79a83bfc39627fc015e5f1
Paul David Tan <cryxuz@gmail.com>
2023-10-10 11:08:59 +1300

    fade effect timeout home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,25 +1,53 @@
-import  Navbar  from './Navbar'
-import home from '/images/home1.jpg'
-
+import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar';
+import home from '/images/home1.jpg';
 
 const Home = () => {
-  
+  const [isH1Visible, setIsH1Visible] = useState(false);
+  const [isPVisible, setIsPVisible] = useState(false);
+
+  useEffect(() => {
+    const h1Timeout = setTimeout(() => {
+      setIsH1Visible(true);
+    }, 800);
+
+    const pTimeout = setTimeout(() => {
+      setIsPVisible(true);
+    }, 1800);
+
+    return () => {
+      clearTimeout(h1Timeout);
+      clearTimeout(pTimeout);
+    };
+  }, []);
+
   return (
     <div>
-      
-      <div className="sticky top-0 xl:bg-fixed bg-center bg-cover text-center w-screen h-screen" style={{ backgroundImage: `url(${home})` }}>
-      <Navbar />
-      <div className="h-screen justify-center pt-[40px] md:pt-[70px] lg:pt-[50px]">
-        
-          <h1 className='text-4xl md:text-5xl lg:text-6xl font-extrabold '>Kia ora. I'm Paul.</h1>
+      <div
+        className="sticky top-0 xl:bg-fixed bg-center bg-cover text-center w-screen h-screen"
+        style={{ backgroundImage: `url(${home})` }}
+      >
+        <Navbar />
+        <div className="home h-screen justify-center pt-[40px] md:pt-[70px] lg:pt-[50px]">
+          <h1
+            className={`text-4xl md:text-5xl lg:text-6xl font-extrabold ${
+              isH1Visible ? 'opacity-100 transition-opacity duration-1000' : 'opacity-0'
+            }`}
+          >
+            Kia ora. I'm Paul.
+          </h1>
           <br />
-          <p className='text-md md:text-2xl lg:text-3xl'>My goal is to provide users with memorable and efficient interactions."</p>
-        
-      </div> 
+          <p
+            className={`text-md md:text-2xl lg:text-3xl ${
+              isPVisible ? 'opacity-100 transition-opacity duration-1000' : 'opacity-0'
+            }`}
+          >
+            My goal is to provide users with memorable and efficient interactions.
+          </p>
+        </div>
       </div>
     </div>
-   
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
