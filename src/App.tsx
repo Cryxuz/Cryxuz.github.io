c7a08f3dad3b53b74779cb454a9ff928407a326d
Paul David Tan <cryxuz@gmail.com>
2023-10-09 19:30:46 +1300

    fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { Home, About, Projects, Contact } from "./components"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import  Home from "./components/Home"
+import Projects from "./components/Projects"
 
 
 function App() {
