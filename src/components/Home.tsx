6218ba7ae42325ce3156a23ffc3903eba2335428
Paul David Tan <cryxuz@gmail.com>
2023-10-10 10:49:45 +1300

    fixed padding

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,9 +9,9 @@ const Home = () => {
       
       <div className="sticky top-0 xl:bg-fixed bg-center bg-cover text-center w-screen h-screen" style={{ backgroundImage: `url(${home})` }}>
       <Navbar />
-      <div className="h-screen justify-center pt-[90px] md:pt-[70px] lg:pt-[50px]">
+      <div className="h-screen justify-center pt-[40px] md:pt-[70px] lg:pt-[50px]">
         
-          <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold '>Kia ora. I'm Paul.</h1>
+          <h1 className='text-4xl md:text-5xl lg:text-6xl font-extrabold '>Kia ora. I'm Paul.</h1>
           <br />
           <p className='text-md md:text-2xl lg:text-3xl'>My goal is to provide users with memorable and efficient interactions."</p>
         
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
       <div>
         <h2 className="py-[10%] font-extrabold text-[42px] md:text-[48px] text-center">Some of my latest work.</h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-[10%] pb-[10%]">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-[10%] pb-[10%] pt-[10%]">
           <div>
             <img className="rounded-xl w-[80%]" src="images/todo.png" alt="" />
           </div>
@@ -28,34 +28,34 @@ const Projects = () => {
             </ul>
           </div>
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-[10%] pb-[10%]">
-          <div>
-          <div>
-            <img className="rounded-xl w-[80%] block md:hidden m-[5%]" src="/images/donut.png" alt="" />
-          </div>
-          <h2 className="text-4xl md:text-[40px] font-semibold hover:text-emerald-400"><a href="https://tiffs-donuts.devacademy.nz/" target="_blank">Tiffs Donut <span className="text-2xl">(Group Project)</span></a></h2>
-          <br />
-            <p className="md:text-2xl">
-              This donut shop group project primarily centers around the acquisition and application of skills related to threeJS and tailwind, with a strong emphasis on learning and practical application. </p>
-            <br /> 
-            <ul className="hidden lg:flex flex-wrap items-center space-x-2 gap-[5px]">
-              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ HTML</li>
-              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ CSS</li>
-              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ JavaScript</li>
-              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ React</li>
-              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ Express</li>
-              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ NodeJs</li>
-              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ TypeScript</li>
-              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ SQLite3</li>
-              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ ThreeJS</li>
-              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ TailwindCSS</li>
-            </ul>
-          </div>
-          <div>
-            <img className="rounded-xl  hidden md:block" src="images/donut.png" alt="" />
-          </div>
-        </div>
+        
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-[10%] pb-[10%] pt-[10%]  ">
+            <div>
+              <div>
+                <img className="rounded-xl w-[80%] block md:hidden my-[14%]" src="/images/donut.png" alt="" />
+              </div>
+              <h2 className="text-4xl md:text-[40px] font-semibold hover:text-emerald-400"><a href="https://tiffs-donuts.devacademy.nz/" target="_blank">Tiffs Donut <span className="text-2xl">(Group Project)</span></a></h2>
+              <br />
+                <p className="md:text-2xl">
+                  This donut shop group project primarily centers around the acquisition and application of skills related to threeJS and tailwind, with a strong emphasis on learning and practical application. </p>
+                <br /> 
+                <ul className="hidden lg:flex flex-wrap items-center space-x-2 gap-[5px]">
+                  <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ HTML</li>
+                  <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ CSS</li>
+                  <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ JavaScript</li>
+                  <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ React</li>
+                  <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ Express</li>
+                  <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ NodeJs</li>
+                  <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ TypeScript</li>
+                  <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ SQLite3</li>
+                  <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ ThreeJS</li>
+                  <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ TailwindCSS</li>
+                </ul>
+              </div>
+              <div>
+                <img className="rounded-xl hidden md:block" src="images/donut.png" alt="" />
+              </div>
+            </div>
       </div>
     </div>
   )
