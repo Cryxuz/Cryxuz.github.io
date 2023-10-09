f28f0b6446216a61a6648e3d76f67fbe14121499
Paul David Tan <cryxuz@gmail.com>
2023-10-09 14:34:47 +1300

    test email

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,98 +1,42 @@
-import React, { useState } from "react";
-import footer from '../assets/footer.jpg'
+// import React, { useRef } from 'react';
+import footer from '../assets/footer.jpg';
 
-const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com'; // Import the 'emailjs-com' library
 
-  const handleSubmit = async (e) => {
+export const Contact = () => {
+  const form = useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log(formData);
-  
-    try {
-      const response = await fetch('/your-api-endpoint', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-  
-      if (response.ok) {
-        // Handle a successful response here
-      } else {
-        // Handle errors here
-      }
-    } catch (error) {
-      // Handle network errors here
+
+    if (form.current) {
+      emailjs.sendForm('service_zgc3dhi', 'template_gjigj6d', form.current, '9Me4x8u4wtwuLriqU')
+        .then((result) => {
+          console.log(result.text);
+        })
+        .catch((error) => {
+          console.log(error.text);
+        });
     }
   };
 
   return (
-    
-  
-    <div className="bg-fixed bg-center bg-cover flex items-center justify-center min-h-screen" style={{backgroundImage: `url(${footer})`}}>
-      <div className="container max-w-4xl bg-slate-700 bg-opacity-30 p-5 mx-5 sm:mt-[100px] sm:mx-[100px] md:p-20 rounded-xl">
-
+    <div className="bg-fixed bg-center bg-cover flex items-center justify-center min-h-screen" style={{ backgroundImage: `url(${footer})` }}>
+       <div className="container max-w-4xl bg-slate-700 bg-opacity-30 p-5 mx-5 sm:mt-[100px] sm:mx-[100px] md:p-20 rounded-xl">
         <h2 className="text-slate-50 font-extrabold text-3xl md:text-[44px] pb-[20px]">Contact Me</h2>
-        <form onSubmit={handleSubmit} className=" rounded-lg  mb-4"> 
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-slate-50 text-md font-bold mb-2">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-slate-200"
-              placeholder="Your name"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-slate-50 text-md font-bold mb-2">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-slate-200"
-              placeholder="Your email"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="message" className="block text-slate-50 text-md font-bold mb-2">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              rows={4}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-slate-200"
-              placeholder="Your message"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex items-center justify-center">
-          <button
-            type="submit"
-            className="bg-slate-700 border-[2px] text-white border-slate-50   hover:bg-slate-600  font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
-            >
-            Submit
-          </button>
-          </div>
-        </form>
-        <h1>INSERT ICONS HERE</h1>
+            <form ref={form} onSubmit={sendEmail}>
+              <label className="block text-slate-50 text-md font-bold mb-2" >Name</label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-slate-200" type="text" name="user_name" />
+              
+              <label className="block text-slate-50 text-md font-bold mb-2" >Email</label>
+              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-slate-200" type="email" name="user_email" />
+              
+              <label className="block text-slate-50 text-md font-bold mb-2">Message</label>
+              <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-slate-200" name="message" />
+              <input type="submit" value="Send" />
+            </form>
       </div>
     </div>
   );
