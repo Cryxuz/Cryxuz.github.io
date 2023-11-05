22caa3da6be49013da6a9fe714213ad802a976fa
Paul David Tan <cryxuz@gmail.com>
2023-11-05 17:28:30 +1300

    add tailored tails ongoing project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -25,6 +25,28 @@ const Projects = () => {
       <div>
         <h2 className="py-[5%] font-extrabold text-[42px] md:text-[48px] text-center">Some of my latest work.</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-[10%] py-[5%]">
+          <div>
+           <a href="https://github.com/Cryxuz/e-commerce-mern" target='_blank'><img style={{ opacity: isInView ? 1 : 0, transition: 'opacity 3s' }} className="mx-auto rounded-xl w-[80%]" src="images/tailoredtails.jpg" alt="tailored-tails project" /></a> 
+          </div>
+          
+          <div>
+          <h2 className="text-center lg:text-left text-4xl md:text-[40px] font-semibold hover:text-emerald-400"><a href="http://paul-tan-to-do-list.devacademy.nz/" target="_blank">Tailored Tails (Pair Programming) (Ongoing)</a></h2>
+          <br />
+            <p className="text-center  lg:text-left md:text-2xl">Tailored Tails, an e-commerce webpage built using the MERN stack, focuses on user authentication & authorization using JWT-auth, user security using bcrypt, and connecting to Stripe</p>
+            <br />
+            <ul className="hidden lg:flex flex-wrap items-center space-x-2 gap-[5px]">
+              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ HTML</li>
+              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ Tailwind CSS</li>
+              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ JavaScript</li>
+              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ React</li>
+              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ Express</li>
+              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ NodeJs</li>
+              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ TypeScript</li>
+              <li className="bg-green-500 rounded-full py-1 px-3 text-white">▷ MongoDB</li>
+            </ul>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-[10%] py-[5%]">
             <div ref={(el) => {
               ref(el);
               elementRef.current = el;
@@ -67,7 +89,7 @@ const Projects = () => {
           </div>
           
           <div>
-          <h2 className="text-center lg:text-left text-4xl md:text-[40px] font-semibold hover:text-emerald-400"><a href="http://paul-tan-to-do-list.devacademy.nz/" target="_blank">Todo List</a></h2>
+          <h2 className="text-center lg:text-left text-4xl md:text-[40px] font-semibold hover:text-emerald-400"><a href="http://paul-tan-to-do-list.devacademy.nz/" target="_blank">Todo List (Solo)</a></h2>
           <br />
             <p className="text-center  lg:text-left md:text-2xl">Introducing my user-friendly todo list, designed to effortlessly assist you in managing your daily tasks and priorities, so you can stay organized and boost your productivity with ease.</p>
             <br />
